b636290aa53f7729de6588c55c625e1b4562ec04
Joel <47868309+findus8000@users.noreply.github.com>
2022-11-08 02:20:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import plotData
 from binance import Client
 
 def main():
-    getBinanceData.main("ETHUSDT", Client.KLINE_INTERVAL_4HOUR, "2 weeks ago UTC")
+    getBinanceData.main("ETHUSDT", Client.KLINE_INTERVAL_4HOUR, "25 days ago UTC")
     generateData.main(30, 20, 0.8)
     plotData.main()
 main()
